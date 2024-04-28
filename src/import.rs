855347b91c608fb90004e27c02d9b8141b2b1054
Malcolm Ramsay <m@malramsay.com>
2024-04-28 18:07:14 +0930

    Use futures_concurrency to add concurrency to the import of files

--- src/import.rs
+++ src/import.rs
@@ -2,6 +2,7 @@ use std::collections::HashMap;
 
 use anyhow::Error;
 use camino::{Utf8Path, Utf8PathBuf};
+use futures_concurrency::prelude::*;
 use itertools::Itertools;
 use sea_orm::DatabaseConnection;
 use walkdir::WalkDir;
@@ -85,8 +86,10 @@ pub fn map_directory_images(directory: &Utf8Path) -> Vec<PictureData> {
         .collect()
 }
 
-// Copy the files from an exisiting location creating a new folder
-// structure.
+/// Copy files from an existing location creating a new folder structure skipping existing files.
+///
+/// This performs a check for the existing files that are within the current database.
+///
 pub async fn import(db: &DatabaseConnection, directory: &Utf8PathBuf) -> Result<(), Error> {
     // Load all existing pictures from the database. We want to do the checks within rust, rather than
     // potentially having large numbers of database queries.
@@ -99,75 +102,21 @@ pub async fn import(db: &DatabaseConnection, directory: &Utf8PathBuf) -> Result<
         .collect::<Vec<_>>();
 
     // To make the lookup process simpler, we first want to convert to a hashmap to make the
-    // act of looking up whether a picture already exists withih the database a quick proces.
+    // act of looking up whether a picture already exists within the database a quick process.
     // Currently this is only using the filename, that is the name given to the file by the camera
     // as the lookup key, however, this lookup process will be improved over time.
+
+    // A complicating factor is the updating of exif metadata. If this gets updated then the files
+    // will not be exactly the same, however the part we are interested in--the image--will be the
+    // same.
+
     // TODO: Use hashes to check for uniqueness within the database.
-    let mut hash_existing: HashMap<String, Vec<PictureData>> = HashMap::new();
-    for picture in pictures_existing.into_iter() {
-        if let Some(f) = hash_existing.get_mut(&picture.filename()) {
-            f.push(picture);
-        } else {
-            hash_existing.insert(picture.filename(), vec![picture]);
-        }
-    }
-
-    // Determine whether the new images we are importing already exist within the database.
-    let mut new_images: Vec<_> = map_directory_images(directory)
-        .into_iter()
-        // TODO: Improve this filter beyond being very basic
-        .filter(|p| !hash_existing.contains_key(&p.filename()))
-        .collect();
-
-    // Parallel map -> Will need to be careful about the directory creation
-    // Spawn async tasks to do the copying?
-    for image in new_images.iter_mut() {
-        tracing::debug!("{:?}", &image);
-        // Create the directory structure
-        let structure = ImportStructure::default();
-
-        let new_path = structure.build_filename(image)?;
-
-        let parent = new_path.parent();
-        tracing::debug!("Importing {} into {}", image.filepath, &new_path);
-
-        // Where the new path is the same as the old one we are actually adding the
-        // file rather than importing, so we can skip all the import steps.
-        if image.filepath != new_path {
-            // Copy to new location
-
-            // Firstly we have to be sure that the directory already exists we are
-            // going to be copying to. This creates the entire directory structure
-            // where it doesn't already exist.
-            std::fs::create_dir_all(parent.unwrap())?;
-
-            // Where there is a file that already exists within the new locaton,
-            // we don't want to overwrite it, which does result in the contents
-            // of the file being removed.
-            if new_path.try_exists().expect("Error checking path exists") {
-                tracing::warn!("File {} already exsits, not copying", &new_path);
-            } else {
-                std::fs::copy(&image.filepath, &new_path).expect("Unable to copy file");
-                // Also copy across the raw file
-                if let Some(ref ext) = image.raw_extension {
-                    std::fs::copy(
-                        &image.filepath.with_extension(ext),
-                        &new_path.with_extension(ext),
-                    )
-                    .expect("Unable to copy file");
-                }
-            }
-        }
-
-        image.filepath = new_path;
-    }
-
-    // Create entry in the database / import
-    add_new_images(db, new_images).await.unwrap();
     // Check the filename
     // Check the capture time
     // Full Method
 
+    // 0. Check the size of the files
+    //     This should just involve reading the metadata
     // 1. Check the short hash of the files
     //     This involves reading just the first n bytes of the new file. This should
     //     be a very quick operation.
@@ -176,6 +125,85 @@ pub async fn import(db: &DatabaseConnection, directory: &Utf8PathBuf) -> Result<
     //     should already be computed.
     // 3. Check the files are equal
     //     Now we also need to read in the old file to check.
+    let hash_existing = tokio::task::spawn_blocking(move || {
+        let mut hash_existing: HashMap<String, Vec<PictureData>> = HashMap::new();
+        for picture in pictures_existing.into_iter() {
+            if let Some(f) = hash_existing.get_mut(&picture.filename()) {
+                f.push(picture);
+            } else {
+                hash_existing.insert(picture.filename(), vec![picture]);
+            }
+        }
+        hash_existing
+    })
+    .await
+    .expect("Issue unwrapping future");
+
+    // Determine whether the new images we are importing already exist within the database.
+    let new_images: Vec<_> = find_directory_images(directory)
+        .into_iter()
+        // TODO: Improve this filter beyond being very basic
+        .filter(|p| !hash_existing.contains_key(&p.filename()))
+        .collect();
+
+    let new_images = new_images
+        // Spawns a concurrent stream to
+        .into_co_stream()
+        .map(|mut image| async move {
+            tracing::debug!("{:?}", &image);
+            // Create the directory structure
+            let structure = ImportStructure::default();
+
+            let new_path = structure
+                .build_filename(&image)
+                .expect("Unable to build filename");
+
+            let parent = new_path.parent();
+            tracing::debug!("Importing {} into {}", image.filepath, &new_path);
+
+            // Where the new path is the same as the old one we are actually adding the
+            // file rather than importing, so we can skip all the import steps.
+            if image.filepath != new_path {
+                // Copy to new location
+
+                // Firstly we have to be sure that the directory already exists we are
+                // going to be copying to. This creates the entire directory structure
+                // where it doesn't already exist.
+                // Within tokio this is guaranteed not to fail in a race condition with
+                // itself. https://docs.rs/tokio/latest/tokio/fs/fn.create_dir_all.html
+                tokio::fs::create_dir_all(parent.unwrap())
+                    .await
+                    .expect("Unable to create directory.");
+
+                // Where there is a file that already exists within the new locaton,
+                // we don't want to overwrite it, which does result in the contents
+                // of the file being removed.
+                if new_path.try_exists().expect("Error checking path exists") {
+                    tracing::warn!("File {} already exists, not copying", &new_path);
+                } else {
+                    tokio::fs::copy(&image.filepath, &new_path)
+                        .await
+                        .expect("Unable to copy file");
+                    // Also copy across the raw file
+                    if let Some(ref ext) = image.raw_extension {
+                        tokio::fs::copy(
+                            &image.filepath.with_extension(ext),
+                            &new_path.with_extension(ext),
+                        )
+                        .await
+                        .expect("Unable to copy file");
+                    }
+                }
+            }
+
+            image.filepath = new_path;
+            image
+        })
+        .collect()
+        .await;
+
+    // Create entry in the database / import
+    add_new_images(db, new_images).await.unwrap();
     Ok(())
 }
 
@@ -188,7 +216,7 @@ pub async fn find_new_images(db: &DatabaseConnection, directory: &Utf8PathBuf) {
     );
 
     let dir = directory.clone();
-    let images: Vec<_> = map_directory_images(&dir)
+    let images: Vec<_> = find_directory_images(&dir)
         .into_iter()
         .filter(|p| !existing_pictures.contains(&p.filepath))
         .collect();
@@ -196,9 +224,8 @@ pub async fn find_new_images(db: &DatabaseConnection, directory: &Utf8PathBuf) {
     if images.is_empty() {
         tracing::info!("No new images found in directory {directory}");
         return;
-    } else {
-        tracing::info!("Adding {} new images to the database.", images.len());
     }
+    tracing::info!("Adding {} new images to the database.", images.len());
 
     add_new_images(db, images).await.unwrap();
 }
